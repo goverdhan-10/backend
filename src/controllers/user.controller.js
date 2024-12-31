056cd0a9d46a7c92b3326656da8a1f9dbea355fd
goverdhan-10 <gupta.goverdhan7253@gmail.com>
2024-12-31 22:40:42 +0530

    setup 2

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -19,7 +19,6 @@ const registerUser = asyncHandler( async (req, res) => {
 
 //getting data from frontend
     const {fullname, email, username, password} = req.body
-    console.log("email:", email)
 //validation
     if(
         [fullname, email, username, password].some((field) => field?.trim() === "")
@@ -27,7 +26,7 @@ const registerUser = asyncHandler( async (req, res) => {
         throw new ApiError(400, "All fields are required")
     }
 //existing user
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or: [{username},{email}]
     })
 
@@ -38,8 +37,11 @@ const registerUser = asyncHandler( async (req, res) => {
 //check for images
 
     const avatarLocalPath = req.files?.avatar[0]?.path
-    const coverImageLocalPath = req.files?.coverImage[0]?.path
-
+    //const coverImageLocalPath = req.files?.coverImage[0]?.path
+    let coverImageLocalPath;
+    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length>0){
+        coverImageLocalPath = req.files.coverImage[0].path
+    }
     if(!avatarLocalPath) throw new ApiError(400, "Avatar is required")
 
 //upload on cloudinary
